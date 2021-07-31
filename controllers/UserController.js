374b4339f2c7d8c4ff62aa736d09527a0b8cd6be
CRISTIANO DA SILVA FERREIRA <68359459+CristianoDaSilvaFerreira@users.noreply.github.com>
2021-07-31 16:04:34 -0300

    :up:  Usando o Dataset
    Para que o DataSet funcione corretamente é necessário usar também o JSON  com suas respectivas propriedade como JSON.stringify e JSON.parse

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -22,6 +22,8 @@ class UserController {
 
       let values = this.getValues();
 
+      if (!values) return false;
+
       // Carregando a foto
       this.getPhoto().then(
         (content) => {
@@ -133,6 +135,8 @@ class UserController {
 
     let tr = document.createElement('tr');
 
+    tr.dataset.user = JSON.stringify(dataUser);
+
     tr.innerHTML = 
     `
       <td>
@@ -149,6 +153,27 @@ class UserController {
     `;
 
     this.tableEl.appendChild(tr);
+
+    this.updateCount();
+  }
+
+  updateCount() {
+
+    let numberUsers = 0;
+    let numberAdmin = 0;
+
+    [...this.tableEl.children].forEach(tr => {
+
+      numberUsers++;
+
+      let user = JSON.parse(tr.dataset.user);
+
+      if (user._admin) numberAdmin++;
+
+    });
+
+    document.querySelector('#number-users').innerHTML = numberUsers;
+    document.querySelector('#number-users-admin').innerHTML = numberAdmin;
   }
 
 }
