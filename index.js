3d8c32eda6177acfb368ac8925a68146bf6aacdc
CRISTIANO DA SILVA FERREIRA <68359459+CristianoDaSilvaFerreira@users.noreply.github.com>
2021-07-31 00:25:16 -0300

    :up: removendo a tabela do HTML e criando ela dinâmicamente
    Fazendo o uso do tamplete string, cria-se uma forma de remover a tabela do formulário do HTML, e criando ela de forma dinâmica diretamente do JavaScript.

--- index.js
+++ index.js
@@ -2,28 +2,50 @@ var fields = document.querySelectorAll('#form-user-create [name]');
 
 var user = {};
 
-fields.forEach(function(field, index) {
+function addLine(dataUser) {
 
-  if (field.name == 'gender') {
+  var tr = document.createElement('tr');
 
-    if (field.checked) {
-      user[field.name] = field.value;
-    }
+  tr.innerHTML = `
+    <tr>
+      <td>
+        <img src="dist/img/user2-160x160.jpg" alt="User Image" class="img-circle img-sm">
+          </td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${dataUser.admin}</td>
+            <td>${dataUser.birth}</td>
+            <td>
+              <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+              <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+        </td>
+    </tr>
+  `;
 
-  } else {
+  document.getElementById('table-users').appendChild(tr);
+}
 
-    user[field.name] = field.value;
-  }
-
-});
-
-console.log(user);
-
-document.getElementById('form-user-create').addEventListener('submit', function(event){
+document.getElementById('form-user-create').addEventListener('submit', function (event) {
 
   event.preventDefault();
 
-  console.log('ok');
+  fields.forEach(function (field, index) {
+
+    if (field.name == 'gender') {
+
+      if (field.checked) {
+        user[field.name] = field.value;
+      }
+
+    } else {
+
+      user[field.name] = field.value;
+    }
+
+  });
+
+  addLine(user);
 
 });
+
 
