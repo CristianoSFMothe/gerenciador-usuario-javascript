f74be7ea3d6c5b1728a320ef9cb8575d970872d5
CRISTIANO DA SILVA FERREIRA <68359459+CristianoDaSilvaFerreira@users.noreply.github.com>
2021-07-31 14:43:35 -0300

    :up: Validando o formulário

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -85,8 +85,15 @@ class UserController {
   getValues() {
 
     let user = {};
+    let isValid = true;
 
     [...this.formEl.elements].forEach(function (field, index) {
+
+      if (['name', 'email', 'password'].indexOf(field.name) > -1 && !field.value) {
+
+        field.parentElement.classList.add('has-error');
+        isValid = false;
+      }
 
       if (field.name == 'gender') {
 
@@ -101,9 +108,13 @@ class UserController {
       } else {
 
         user[field.name] = field.value;
-      }
+      }      
 
     });
+
+    if (!isValid) {
+      return false;
+    }
 
     return new User(
       user.name,
